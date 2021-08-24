4091c737af2b879cf546ec4e5691030f9728d81a
Webioann <ioannn7777@gmail.com>
2021-08-24 14:47:46 +0300

    install all moduls needed for webpack

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 ## Let start
         $npm install -D webpack webpack-cli
+        $npm install -D style-loader css-loader sass-loader file-loader
+        $ npm install -D html-webpack-plugin clean-webpack-plugin mini-css-extract-plugin
+        $ npm install webpack-dev-server
+2. create directory **src /../../** <br>
+
+
+
+
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "[name].[hash].js", 
+        path: path.resolve(__dirname,"dist")
+    },
+    devServer: {
+        // historyApiFallback: true,
+        // contentBase: path.resolve(__dirname, 'index.js'),
+        // open: true,
+        // compress: true,
+        // hot: true,
+        port: 3000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css', 
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use:[MiniCssExtractPlugin.loader,'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use:[MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+            },
+            // {
+            //     test: /\.(js|jsx)$/,
+            //     exclude: /node_modules/,
+            //     use: {loader: "babel-loader"}
+            // },    
+            {
+                test: /\.(svg|png|jpg|gif|jpeg|ico)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
+}
