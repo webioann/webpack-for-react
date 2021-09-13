c4be995a59139961b7069f66462b4e42e4f25e69
Webioann <ioannn7777@gmail.com>
2021-09-13 23:10:03 +0300

    install icons for Contats

--- src/COMPONENTS/Contacts.js
+++ src/COMPONENTS/Contacts.js
@@ -6,13 +6,29 @@ function Contacts() {
         <section className="contacts row">
             <a name="contacts" href="#"></a>
             <div className="col-sm-6 col-lg-3">
-                Contact with information</div>
+                <div className="row">
+                    <div className="viber-icon col-2"></div>
+                    <div className="col-10">Contact with information</div>
+                </div>
+            </div>
             <div className="col-sm-6 col-lg-3">
-                Contact with information</div>
+                <div className="row">
+                    <div className="linkedin-icon col-2"></div>
+                    <div className="col-10">Contact with information</div>
+                </div>
+            </div>
             <div className="col-sm-6 col-lg-3">
-                Contact with information</div>
-            <div className="col-sm-6 col-lg-3">
-                Contact with information</div>
+                <div className="row">
+                    <div className="gmail-icon col-2"></div>
+                    <div className="col-10">Contact with information</div>
+                </div>
+            </div>
+            <div className="git-hub col-sm-6 col-lg-3">
+                <div className="row">
+                    <div className="git-hub-icon col-2"></div>
+                    <div className="col-10">Contact with information</div>
+                </div>
+            </div>
         </section>
     )
 }
