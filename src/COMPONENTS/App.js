1271c031a84b210ae3f79c10d263265ea92471c0
webioann <ioannn7777@gmail.com>
2021-09-29 22:24:32 +0300

    use svg blobs for design

--- src/COMPONENTS/App.js
+++ src/COMPONENTS/App.js
@@ -7,6 +7,9 @@ import Projects from './Projects.js'
 import Contacts from './Contacts.js'
 import './app.scss'
 
+import blob from '../assets/blob.svg'
+import blob3 from '../assets/blob3.svg'
+
 function App() {
 
   const langMode = useSelector(state => state.langMode.lang)
@@ -25,6 +28,11 @@ function App() {
   console.log(langMode,lang);
   return (
     <div className="portfolio-app">
+      <div className="container blob-box">
+        <div className="blob"></div>
+          <img src={blob} className="blob-svg" alt="blob svg" />
+          <img src={blob3} className="blob3-svg" alt="blob2 svg" />
+      </div>
       <Navbar/>
       <div className="app container">
           <Summary/>
