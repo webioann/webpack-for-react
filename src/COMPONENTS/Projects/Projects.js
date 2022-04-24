06f9406a11a99571ff21ff1f9257af91e53ffc18
webioann <ioannn7777@gmail.com>
2022-04-24 13:32:05 +0300

    choose branches

--- src/COMPONENTS/Projects/Projects.js
+++ src/COMPONENTS/Projects/Projects.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import useTranslator from '../../hooks/useTranslator';
-import { projTitle,portfolio,dashboard } from '../../data/multiLang';
+import { portfolio,dashboard } from '../../data/multiLang';
 import { gitHubPortfolio,gitHubDashboard } from '../../data/constants';
 import LinkBox from '../LinkBox/LinkBox';
 // ======== image imports ==============================
 import desktopDark from '../../assets/desktop-dk-eng.png'
-import desktopLight from '../../assets/desktop-lt-rus.png'
+// import desktopLight from '../../assets/desktop-lt-rus.png'
 import tabletDark from '../../assets/tablet-dk-eng.png'
-import tabletLight from '../../assets/tablet-lt-rus.png'
+// import tabletLight from '../../assets/tablet-lt-rus.png'
 import mobileDark from '../../assets/mobile-dk-eng.png'
-import mobileLight from '../../assets/mobile-lt-eng.png'
+// import mobileLight from '../../assets/mobile-lt-eng.png'
 import './projects.scss';
 
 function Projects() {
