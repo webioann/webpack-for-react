cf171a906a2a9f8ed116ad7691d9a0d37e607fe6
webioann <ioannn7777@gmail.com>
2022-07-25 10:13:31 +0300

    fix delay interval

--- src/data/tools_portfolio.js
+++ src/data/tools_portfolio.js
@@ -1,9 +1,9 @@
 export const tools_portfolio = [
-    {title: 'React JS',           id: 1,  delay: "400ms" , textEng: 'text for tooltip'},
-    {title: 'Redux',              id: 2,  delay: "1000ms", textEng: 'text for tooltip'},
-    {title: 'custom hooks',       id: 3,  delay: "1900ms", textEng: 'text for tooltip'},
-    {title: 'Webpack',            id: 4,  delay: "2200ms", textEng: 'text for tooltip'},
-    {title: 'Sass',               id: 5,  delay: "2200ms", textEng: 'text for tooltip'},
-    {title: 'multi lang UI',      id: 6,  delay: "2200ms", textEng: 'text for tooltip'},
-    {title: 'responsive design',  id: 7,  delay: "2500ms", textEng: 'text for tooltip'},
+    {title: 'React JS',           id: 1,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'Redux',              id: 2,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'custom hooks',       id: 3,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'Webpack',            id: 4,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'Sass',               id: 5,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'multi lang UI',      id: 6,  delay: "0ms", textEng: 'text for tooltip'},
+    {title: 'responsive design',  id: 7,  delay: "0ms", textEng: 'text for tooltip'},
 ];
