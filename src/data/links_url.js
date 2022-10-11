d8da48d4b996a3a70669bd32f62fcb89e2e89e89
webioann <ioannn7777@gmail.com>
2022-10-11 17:21:59 +0300

    eee

--- src/data/links_url.js
+++ src/data/links_url.js
@@ -1,9 +1,9 @@
 
-export const resumeUkr = "https://drive.google.com/file/d/11fG21fDNB0OUszBqt84H9Rd2Hp9tv5s_/view?usp=sharing";
+export const resumeUkr = "https://drive.google.com/file/d/1lIctZUQFPNrR6zXFb3bkN1KeYl49eLMb/view?usp=sharing";
 
-export const resumeRus = "https://drive.google.com/file/d/11qae_y__S1bZz9QaokPRj0uOiuN7oyqq/view?usp=sharing";
+export const resumeRus = "https://drive.google.com/file/d/1-vYFzP7qe7KcnWvx8KgMncAK4CZ8m9IP/view?usp=sharing";
 
-export const resumeEng = "https://drive.google.com/file/d/1kFMjXM92tGwwCO-vjpGiXQRYfl2io4QH/view?usp=sharing";
+export const resumeEng = "https://drive.google.com/file/d/1zLr6kiEIl1GC7waMx9X2PaVuwgSgX6_D/view?usp=sharing";
 
 //===== Contacts ====
 export const phone = '+380(96)842-25-72';
@@ -17,14 +17,14 @@ export const gitHubLink = "https://github.com/webioann";
 //===== GIT-HUB ====
 export const users_githublink = "https://github.com/webioann/random_users/";
 
-export const crypto_githublink = "https://github.com/webioann/cryptocurrency";
+export const crypto_githublink = "https://github.com/webioann/cryptovers";
 
 export const portfolio_githublink = "https://github.com/webioann/portfolio/";
 
 //===== SITE =====
 export const randomusers_link = "https://webioann.github.io/random_users/";
 
-export const cryptocurrency_link = "https://cryptocurrency-cea64.web.app/";
+export const cryptocurrency_link = "https://ioanncryptovers.web.app/";
 
 export const linkText = {
     site: {
--- src/data/text_for_cryptocurrency.js
+++ src/data/text_for_cryptocurrency.js
@@ -8,15 +8,15 @@ export const cryptocurrency = {
         ukr: 'Опис проекта :'
     },
     discr_text: {
-        eng: 'Cryptocurrency is a full-stack application based on CoinGecko API and Firebase. Four currencies. Search for cryptocurrencies among all 13,000 existing ones. Provides detailed statistical information on a single currency - dynamic graphs displaying data for 8 different periods, articles and links to social networks. It has a customer authorization system through Firebase-auth and Google for entering the personal account. Allows the client to create, edit and save "watch lists" on the server, using the Firebase Database SDK. Server-side pagination in an interesting implementation and design. The design is made in two color themes and the application is fully adaptive for all devices.',
-        ukr: 'Cryptocurrency - це фулстек додаток на основі CoinGecko API та Firebase. Чотири валюти. Пошук криптовалют серед всіх 13 000 існуючих. Надає детальну статистичну інформацію по окремій валюті - динамічні графіки що виводять дані за 8 різних періодів, статті та посиланнями на соцмережі. Має систему авторизації клієнтів через Firebase-auth і Google для входу в особистий кабінет. Дозволяє клієнту створювати, редагувати та зберігати "списки переглядів" на сервері, з допомогою SDK на Firebase Database. Пагинація на боці сервера в цікавій реалізації та дизайні. Дизайн зроблено в двох кольорових темах та повною адапивністю додатка під всі пристрої.',
+        eng: 'Cryptocurrency is a application based on CoinGecko API and Firebase. Fresh news from the world of cryptocurrencies, as well as separate news for each currency. Four currencies. Search for cryptocurrencies among all 13,000 existing ones. Provides detailed statistical information on a single currency - dynamic graphs displaying data for 8 different periods, articles and links to social networks. Server-side pagination in an interesting implementation and design. The design is made in two color themes and the application is fully adaptive for all devices.',
+        ukr: 'Cryptocurrency - це додаток на основі CoinGecko API та Firebase. Свіжі новини зі світу криптовалют, а також окремі новини для кожної валюти. Чотири валюти. Пошук криптовалют серед всіх 13 000 існуючих. Надає детальну статистичну інформацію по окремій валюті - динамічні графіки що виводять дані за 8 різних періодів, статті та посиланнями на соцмережі. Пагинація на боці сервера в цікавій реалізації та дизайні. Дизайн зроблено в двох кольорових темах та повною адапивністю додатка під всі пристрої.',
     },
     used_tools_span: {
         eng: 'Used tools :',
         ukr: 'Використав :'
     },
     used_tools_text: {
-        eng: 'React, TypeScript, Redux, React-router, RTK Query, Firebase JWT for authentication, SDK, REST API, Webpack, Git-hub, Sass, CSS-animation, responsive design (mobile-first).',
-        ukr: 'React, TypeScript, Redux, React-router, RTK Query, Firebase JWT для аунтефікації, SDK, REST API, Webpack, Git-hub, Sass, CSS-анімація, адаптивний дизайн (mobile-first).'
+        eng: 'React, TypeScript, Redux, React-router, RTK Query, Firebase SDK, REST API, Webpack, Git-hub, Sass, CSS-animation, responsive design (mobile-first).',
+        ukr: 'React, TypeScript, Redux, React-router, RTK Query, Firebase SDK, REST API, Webpack, Git-hub, Sass, CSS-анімація, адаптивний дизайн (mobile-first).'
     },
 };
