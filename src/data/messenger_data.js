5afeb7ef0545c1328b5392480a38f602d9ff886d
Webioann <ioannn7777@gmail.com>
2024-03-04 19:27:09 +0200

    created discription for messenger_data

--- src/data/messenger_data.js
+++ src/data/messenger_data.js
@@ -1,10 +1,10 @@
 export const messenger_data = {
     discription: {
-        eng: 'Cryptocurrency is a crypto informer based on the CoinGecko API. Provides statistical information on all cryptocurrencies and in more detail on each individual currency with dynamic graphs displaying data for 8 different periods, articles and links to social networks. Price conversion for four currencies. Search for cryptocurrencies among all 13,000 existing ones. Fresh news from the world of cryptocurrencies, as well as separate news for each currency. Server-side pagination in an interesting implementation and design. The design is made in two color themes and the application is fully adaptive for all devices.',
-        ukr: 'Cryptocurrency - це крипто-інформер на основі CoinGecko API.  Надає статистичну інформацію по всім криптовалютам і більш детально по кожній окремій валюті з динамічними графіками що виводять дані за 8 різних періодів, статті та посиланнями на соцмережі.  Конвертація цін для чотирьох валют. Пошук криптовалют серед всіх 13 000 існуючих. Свіжі новини зі світу криптовалют, а також окремі новини для кожної валюти.  Пагинація на боці сервера в цікавій реалізації та дизайні. Дизайн зроблено в двох кольорових темах та повною адапивністю додатка під всі пристрої.',
+        eng: 'Messenger is a full-stack mobile messenger written in React Native. Allows users to exchange text messages, pictures and photos, create new groups, edit their own profile information. Has full authentication through e-mail and Google account. Uses Firebase NoSQL database to store messages and user data on the server.',
+        ukr: 'Messenger - це фулстек  мобільний меседжер написаний на React Native. Дозволяє обмінюватися користувачам текстовими повідомленнями, картинками і фото, створювати нові групи, редагувати відомості власного профілю. Має повноцінну автентифікацію через електронну пошту і акаунт Google. Використовує Firebase NoSQL бази даних для збереження повідомлень  і даних користувача на сервері.',
     },
-    site_link: 'https://ioanncryptovers.web.app/',
-    github_link: 'https://github.com/webioann/cryptovers',
+    site_link: '',
+    github_link: 'https://github.com/webioann/Messenger',
     tools: [
         { title: 'React Native', id: 1, delay: '0ms', textEng: 'text for tooltip' },
         {
