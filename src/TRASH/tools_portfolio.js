49698f1de2572d2df5a2ec14b06799006c0f17a8
Webioann <ioannn7777@gmail.com>
2024-03-04 20:54:08 +0200

    delete Trash components folder

--- src/TRASH/tools_portfolio.js
+++
@@ -1,9 +0,0 @@
-export const tools_portfolio = [
-    {title: 'React JS',           id: 1,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'Redux',              id: 2,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'custom hooks',       id: 3,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'Webpack',            id: 4,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'Sass',               id: 5,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'multi lang UI',      id: 6,  delay: "0ms", textEng: 'text for tooltip'},
-    {title: 'responsive design',  id: 7,  delay: "0ms", textEng: 'text for tooltip'},
-];
